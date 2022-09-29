5ec900125615e1861bd49d93f97f7b3025fa5d68
Binita Gupta <binitagupta24@gmail.com>
2022-09-28 22:12:58 -0400

    Enable different colors for different hard-coded availabilities and add comments

--- quickstart/src/App.js
+++ quickstart/src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import "./styles.css";
 import ScheduleSelector from "react-schedule-selector";
 
+// Create React component for the scheduling view
 class App extends React.Component {
   state = { schedule: [] };
 
+  // Set state when schedules are changed
   handleChange = (newSchedule) => {
     this.setState({ schedule: newSchedule });
   };
 
+  /* Return React app with two side-by-side calendar views. Show 5 days, 8am-9pm, in 1-hour increments. */
   render() {
     return (
       <div className="App">
         <h1>When2Meet</h1>
         <div class="schedules">
           <div class="cal">
+            <h2>Your Availability</h2>
             <ScheduleSelector
               selection={this.state.schedule}
               numDays={5}
@@ -24,7 +28,8 @@ class App extends React.Component {
               onChange={this.handleChange}
             />
           </div>
-          <div class="cal">
+          <div class="cal" id="group">
+            <h2>Group Availability</h2>
             <ScheduleSelector
               selection={this.state.schedule}
               numDays={5}
